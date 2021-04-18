87606aeb26cef032ff50493cb2b4aa2b2eda32cd
Adolpho Piazza <adolpho@premiersoft.net>
2021-04-17 23:59:09 -0300

    Setting up project to use view code instead of storyboard

--- ViewCodeObjC/SceneDelegate.m
+++ ViewCodeObjC/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -15,9 +16,14 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    self.window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    self.window.windowScene = (UIWindowScene *)scene;
+    
+    ViewController *viewController = [[ViewController alloc] init];
+    viewController.view.backgroundColor = [UIColor systemBackgroundColor];
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController: viewController];
+    [self.window makeKeyAndVisible];
 }
 
 
--- ViewCodeObjC/ViewController.m
+++ ViewCodeObjC/ViewController.m
@@ -7,7 +7,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    UILabel *helloWorldLabel;
+    UIButton *touchMeButton;
+}
 
 @end
 
@@ -15,8 +18,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupView];
+    [self setupHelloWorldLabel];
+    [self setupTouchMeButton];
 }
 
+- (void)setupView {
+    self.title = @"ViewCode with Objective-C";
+}
+
+- (void)setupHelloWorldLabel {
+    helloWorldLabel = [[UILabel alloc] init];
+    [helloWorldLabel setTranslatesAutoresizingMaskIntoConstraints: NO];
+    [self.view addSubview: helloWorldLabel];
+    
+    [helloWorldLabel setText: @"Hello Objective-C with ViewCode!"];
+    [helloWorldLabel setFont: [UIFont systemFontOfSize: 30]];
+    [helloWorldLabel setTextColor: [UIColor labelColor]];
+    [helloWorldLabel setTextAlignment: NSTextAlignmentCenter];
+    [helloWorldLabel setNumberOfLines: 0];
+    
+    [[helloWorldLabel.topAnchor
+      constraintEqualToAnchor: self.view.safeAreaLayoutGuide.topAnchor constant: 100] setActive: YES];
+    [[helloWorldLabel.leadingAnchor constraintEqualToAnchor: self.view.leadingAnchor constant: 15] setActive: YES];
+    [[helloWorldLabel.trailingAnchor constraintEqualToAnchor: self.view.trailingAnchor constant: -15] setActive: YES];
+}
+
+- (void)setupTouchMeButton {
+    touchMeButton = [[UIButton alloc] init];
+    [touchMeButton setTranslatesAutoresizingMaskIntoConstraints: NO];
+    [self.view addSubview: touchMeButton];
+    
+    [touchMeButton setTitle: @"Touch Me" forState: UIControlStateNormal];
+    [touchMeButton setTitleColor: [UIColor labelColor] forState: UIControlStateNormal];
+    [[touchMeButton titleLabel] setFont: [UIFont systemFontOfSize: 25 weight: UIFontWeightSemibold]];
+    [touchMeButton setBackgroundColor: [UIColor secondarySystemBackgroundColor]];
+    [[touchMeButton layer] setCornerRadius: 10];
+    [touchMeButton addTarget: self action: @selector(buttonAction) forControlEvents: UIControlEventTouchUpInside];
+    
+    [[touchMeButton.centerYAnchor constraintEqualToAnchor: self.view.centerYAnchor] setActive: YES];
+    [[touchMeButton.leadingAnchor constraintEqualToAnchor: self.view.leadingAnchor constant: 15] setActive: YES];
+    [[touchMeButton.trailingAnchor constraintEqualToAnchor: self.view.trailingAnchor constant: -15] setActive: YES];
+    [[touchMeButton.heightAnchor constraintEqualToConstant: 50] setActive: YES];
+}
+
+- (void)buttonAction {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle: @"View Code"
+                                                  message: @"Objective-C can be fun!"
+                                                  preferredStyle: UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle: @"YES" style: UIAlertActionStyleDefault handler: nil];
+    [alert addAction: action];
+    [self.navigationController presentViewController: alert animated: YES completion: nil];
+}
 
 @end
